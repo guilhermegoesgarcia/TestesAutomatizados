da19cafaa360656d12b75207a4ab1f92909a27c1
guilherme <guilherme.goes.garcia@hotmail.com>
2021-08-19 17:31:34 -0300

    ok

--- pacote_testes/test_usuario.py
+++ pacote_testes/test_usuario.py
@@ -8,3 +8,24 @@ def test_deve_subtrair_valor_da_carteira_do_usuario_quando_este_propor_um_lance(
 
     #esperamos que retorne:
     assert vini.carteira == 50.0
+
+def test_deve_permitir_propor_lance_quando_o_valor_e_menor_do_que_o_valor_da_carteira():
+    vini = Usuario('Vini', 100.0)
+    leilao = Leilao('Celular')
+    vini.proproe_lance(leilao,1.0)
+
+    assert vini.carteira == 99.0
+
+def test_deve_permitir_quando_o_lance_e_igual_valor_da_carteira():
+    vini = Usuario('Vini',100.0)
+    leilao = Leilao('Celular')
+    vini.propoe_lance(leilao,100.0)
+
+    assert vini.carteira == 0.0
+
+def test_nao_deve_permitir_propor_lance_maior_que_contido_na_carteira():
+    vini = Usuario('Vini',100.0)
+    leilao = Leilao('Celular')
+    vini.propoe_lance(leilao,200.0)
+
+    assert vini.carteira == 100.0
